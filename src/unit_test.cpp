b31adc5869f2c351d7737810169db55d59ace85f
Raphi-Sch <raphael.scharff@gmail.com>
2021-02-01 20:33:07 +0100

    * refactor test

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -10,122 +10,33 @@ void run_all_test(CPU& cpu, Memory& mem){
     mem.write(0xfffc, 0x00);
     mem.write(0xfffd, 0x02);
 
-    nb_test++; if(LDA_IM(cpu, mem)) test_passed++;
-    nb_test++; if(LDA_ZP(cpu, mem)) test_passed++;
-    nb_test++; if(LDA_ZPX(cpu, mem)) test_passed++;
-    nb_test++; if(LDA_ABS(cpu, mem)) test_passed++;
-    nb_test++; if(LDA_ABSX(cpu, mem)) test_passed++;
-    nb_test++; if(LDA_ABSY(cpu, mem)) test_passed++;
+    // LDA
+    nb_test++; if(load_register_immediate(cpu, mem, CPU::INS_LDA_IM)) test_passed++;
+    nb_test++; if(load_register_zero_page(cpu, mem, CPU::INS_LDA_ZP)) test_passed++;
+    nb_test++; if(load_register_zero_page_X(cpu, mem, CPU::INS_LDA_ZPX)) test_passed++;
+    nb_test++; if(load_register_absolute(cpu, mem, CPU::INS_LDA_ABS)) test_passed++;
+    nb_test++; if(load_register_absolute_X(cpu, mem, CPU::INS_LDA_ABSX)) test_passed++;
+    nb_test++; if(load_register_absolute_Y(cpu, mem, CPU::INS_LDA_ABSY)) test_passed++;
     nb_test++; if(LDA_INDX(cpu, mem)) test_passed++;
     nb_test++; if(LDA_INDY(cpu, mem)) test_passed++;
+
+    // LDX
+    nb_test++; if(load_register_immediate(cpu, mem, CPU::INS_LDX_IM)) test_passed++;
+    nb_test++; if(load_register_zero_page(cpu, mem, CPU::INS_LDX_ZP)) test_passed++;
+    nb_test++; if(load_register_absolute(cpu, mem, CPU::INS_LDX_ABS)) test_passed++;
+    nb_test++; if(load_register_absolute_Y(cpu, mem, CPU::INS_LDX_ABSY)) test_passed++;
     
-    cout << test_passed << " of " << nb_test << " test passed successfully" << endl;
+    // LDY
+    nb_test++; if(load_register_immediate(cpu, mem, CPU::INS_LDY_IM)) test_passed++;
+    nb_test++; if(load_register_zero_page(cpu, mem, CPU::INS_LDY_ZP)) test_passed++;
+    nb_test++; if(load_register_absolute(cpu, mem, CPU::INS_LDY_ABS)) test_passed++;
+    nb_test++; if(load_register_absolute_X(cpu, mem, CPU::INS_LDY_ABSX)) test_passed++;
+    
+    cout << dec << test_passed << " of " << nb_test << " test passed successfully" << endl;
 }
 
-bool LDA_IM(CPU& cpu, Memory& mem){
-    bool valid = true;
+// LDA
 
-    // Reseting cpu
-    cpu.reset(mem);
-    
-    CPU CpuCopy = cpu;
-
-    // ZeroFlag shoud be false, NegativeFlag shoud be true
-    mem.write(0x0200, CPU::INS_LDA_IM);
-    mem.write(0x0201, 0xF1);
-
-    cpu.step_run(mem);
-
-    if(!expected_eq(cpu.Accumulator, 0xF1, "INS_LDA_IM", "Accumulator")) valid = false;
-    if(!expected_eq(cpu.ZeroFlag, false, "INS_LDA_IM", "ZeroFlag")) valid = false;
-    if(!expected_eq(cpu.NegativeFlag, true, "INS_LDA_IM", "NegativeFlag")) valid = false;
-    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, "INS_LDA_IM")) valid = false;
-
-    // Reseting cpu
-    cpu.reset(mem);
-    CpuCopy = cpu;
-
-    // ZeroFlag shoud be true, NegativeFlag shoud be false
-    mem.write(0x0200, CPU::INS_LDA_IM);
-    mem.write(0x0201, 0x00);
-
-    cpu.step_run(mem);
-
-    if(!expected_eq(cpu.Accumulator, 0x00, "INS_LDA_IM", "Accumulator")) valid = false;
-    if(!expected_eq(cpu.ZeroFlag, true, "INS_LDA_IM", "ZeroFlag")) valid = false;
-    if(!expected_eq(cpu.NegativeFlag, false, "INS_LDA_IM", "NegativeFlag")) valid = false;
-    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, "INS_LDA_IM")) valid = false;
-
-    return valid;
-}
-
-bool LDA_ZP(CPU& cpu, Memory& mem){
-    bool valid = true;
-
-    // Reseting cpu
-    cpu.reset(mem);
-    
-    CPU CpuCopy = cpu;
-
-    mem.write(0x0200, CPU::INS_LDA_ZP);
-    mem.write(0x0201, 0x01);
-    mem.write(0x0001, 0xF2);
-
-    cpu.step_run(mem);
-
-    if(!expected_eq(cpu.Accumulator, 0xF2, "INS_LDA_ZP", "Accumulator")) valid = false;
-    if(!expected_eq(cpu.ZeroFlag, false, "INS_LDA_ZP", "ZeroFlag")) valid = false;
-    if(!expected_eq(cpu.NegativeFlag, true, "INS_LDA_ZP", "NegativeFlag")) valid = false;
-    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, "INS_LDA_ZP")) valid = false;
-
-    return valid;
-}
-
-bool LDA_ZPX(CPU& cpu, Memory& mem){
-    bool valid = true;
-
-    // Reseting cpu
-    cpu.reset(mem);
-    
-    CPU CpuCopy = cpu;
-
-    mem.write(0x0200, CPU::INS_LDA_ZPX);
-    mem.write(0x0201, 0x01);
-    mem.write(0x000A, 0xF3);
-    cpu.registerX = 0x09;
-
-    cpu.step_run(mem);
-
-    if(!expected_eq(cpu.Accumulator, 0xF3, "INS_LDA_ZPX", "Accumulator")) valid = false;
-    if(!expected_eq(cpu.ZeroFlag, false, "INS_LDA_ZPX", "ZeroFlag")) valid = false;
-    if(!expected_eq(cpu.NegativeFlag, true, "INS_LDA_ZPX", "NegativeFlag")) valid = false;
-    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, "INS_LDA_ZPX")) valid = false;
-
-    return valid;
-}
-
-bool LDA_ABS(CPU& cpu, Memory& mem){
-    bool valid = true;
-
-    // Reseting cpu
-    cpu.reset(mem);
-    
-    CPU CpuCopy = cpu;
-
-    mem.write(0x0200, CPU::INS_LDA_ABS);
-    mem.write(0x0201, 0x10);
-    mem.write(0x0202, 0x10);
-    mem.write(0x1010, 0xF4);
-
-    cpu.step_run(mem);
-
-    if(!expected_eq(cpu.Accumulator, 0xF4, "INS_LDA_ABS", "Accumulator")) valid = false;
-    if(!expected_eq(cpu.ZeroFlag, false, "INS_LDA_ABS", "ZeroFlag")) valid = false;
-    if(!expected_eq(cpu.NegativeFlag, true, "INS_LDA_ABS", "NegativeFlag")) valid = false;
-    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, "INS_LDA_ABS")) valid = false;
-
-    return valid;
-}
 
 bool LDA_ABSX(CPU& cpu, Memory& mem){
     bool valid = true;
@@ -175,6 +86,8 @@ bool LDA_ABSY(CPU& cpu, Memory& mem){
     return valid;
 }
 
+
+// LDA specific test
 bool LDA_INDX(CPU& cpu, Memory& mem){
     bool valid = true;
 
@@ -224,6 +137,310 @@ bool LDA_INDY(CPU& cpu, Memory& mem){
 
     return valid;
 }
+
+
+// Load register test
+bool load_register_immediate(CPU& cpu, Memory& mem, byte instruction){
+    bool valid = true;
+    string registerName;
+    string instructionName;
+    byte* cpuRegister;
+
+    switch(instruction){
+        case CPU::INS_LDA_IM:
+            registerName = "Accumulator";
+            instructionName = "INS_LDA_IM";
+            cpuRegister = &cpu.Accumulator;
+            break;
+
+        case CPU::INS_LDX_IM:
+            registerName = "registerX";
+            instructionName = "INS_LDX_IM";
+            cpuRegister = &cpu.registerX;
+            break;
+
+        case CPU::INS_LDY_IM:
+            registerName = "registerY";
+            instructionName = "INS_LDY_IM";
+            cpuRegister = &cpu.registerY;
+            break;
+
+        default:
+            cout << "load_register_immediate doesn't handle : " << hex << (int)instruction << endl;
+            return false;
+    }
+
+    // Reseting cpu
+    cpu.reset(mem);
+    
+    CPU CpuCopy = cpu;
+
+    // ZeroFlag shoud be false, NegativeFlag shoud be true
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0xF1);
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF1, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, false, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, true, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+
+    // Reseting cpu
+    cpu.reset(mem);
+    CpuCopy = cpu;
+
+    // ZeroFlag shoud be true, NegativeFlag shoud be false
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0x00);
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF1, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, true, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, false, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+
+    return valid;
+}
+
+bool load_register_zero_page(CPU& cpu, Memory& mem, byte instruction){
+    bool valid = true;
+    string registerName;
+    string instructionName;
+    byte* cpuRegister;
+
+    switch(instruction){
+        case CPU::INS_LDA_ZP:
+            registerName = "Accumulator";
+            instructionName = "INS_LDA_ZP";
+            cpuRegister = &cpu.Accumulator;
+            break;
+
+        case CPU::INS_LDX_ZP:
+            registerName = "registerX";
+            instructionName = "INS_LDX_ZP";
+            cpuRegister = &cpu.registerX;
+            break;
+
+        case CPU::INS_LDY_ZP:
+            registerName = "registerY";
+            instructionName = "INS_LDY_ZP";
+            cpuRegister = &cpu.registerY;
+            break;
+
+        default:
+            cout << "load_register_zero_page doesn't handle : " << hex << (int)instruction << endl;
+            return false;
+    }
+
+    // Reseting cpu
+    cpu.reset(mem);
+    
+    CPU CpuCopy = cpu;
+
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0x01);
+    mem.write(0x0001, 0xF2);
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF2, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, false, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, true, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+
+    return valid;
+}
+
+bool load_register_zero_page_X(CPU& cpu, Memory& mem, byte instruction){
+    bool valid = true;
+    string registerName;
+    string instructionName;
+    byte* cpuRegister;
+
+    switch(instruction){
+        case CPU::INS_LDA_ZPX:
+            registerName = "Accumulator";
+            instructionName = "INS_LDA_ZPX";
+            cpuRegister = &cpu.Accumulator;
+            break;
+
+        case CPU::INS_LDY_ZPX:
+            registerName = "registerY";
+            instructionName = "INS_LDY_ZPX";
+            cpuRegister = &cpu.registerY;
+            break;
+
+        default:
+            cout << "load_register_zero_page_X doesn't handle : " << hex << (int)instruction << endl;
+            return false;
+    }
+
+    // Reseting cpu
+    cpu.reset(mem);
+    
+    CPU CpuCopy = cpu;
+
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0x01);
+    mem.write(0x000A, 0xF3);
+    cpu.registerX = 0x09;
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF3, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, false, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, true, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+
+    return valid;
+}
+
+bool load_register_absolute(CPU& cpu, Memory& mem, byte instruction){
+    bool valid = true;
+    string registerName;
+    string instructionName;
+    byte* cpuRegister;
+
+    switch(instruction){
+        case CPU::INS_LDA_ABS:
+            registerName = "Accumulator";
+            instructionName = "INS_LDA_ABS";
+            cpuRegister = &cpu.Accumulator;
+            break;
+
+        case CPU::INS_LDX_ABS:
+            registerName = "registerX";
+            instructionName = "INS_LDX_ABS";
+            cpuRegister = &cpu.registerX;
+            break;
+
+        case CPU::INS_LDY_ABS:
+            registerName = "registerY";
+            instructionName = "INS_LDY_ABS";
+            cpuRegister = &cpu.registerY;
+            break;
+
+        default:
+            cout << "load_register_absolute doesn't handle : " << hex << (int)instruction << endl;
+            return false;
+    }
+
+    // Reseting cpu
+    cpu.reset(mem);
+    
+    CPU CpuCopy = cpu;
+
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0x10);
+    mem.write(0x0202, 0x10);
+    mem.write(0x1010, 0xF4);
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF4, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, false, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, true, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+
+    return valid;
+}
+
+bool load_register_absolute_X(CPU& cpu, Memory& mem, byte instruction){
+    bool valid = true;
+    string registerName;
+    string instructionName;
+    byte* cpuRegister;
+
+    switch(instruction){
+        case CPU::INS_LDA_ABSX:
+            registerName = "Accumulator";
+            instructionName = "INS_LDA_ABSX";
+            cpuRegister = &cpu.Accumulator;
+            break;
+
+        case CPU::INS_LDY_ABSX:
+            registerName = "registerY";
+            instructionName = "INS_LDY_ABSX";
+            cpuRegister = &cpu.registerY;
+            break;
+
+        default:
+            cout << "load_register_absolute_X doesn't handle : " << hex << (int)instruction << endl;
+            return false;
+    }
+    // Reseting cpu
+    cpu.reset(mem);
+    
+    CPU CpuCopy = cpu;
+
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0x10);
+    mem.write(0x0202, 0x10);
+    mem.write(0x101A, 0xF5);
+    cpu.registerX = 0x0A;
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF5, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, false, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, true, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+
+    return valid;
+}
+
+bool load_register_absolute_Y(CPU& cpu, Memory& mem, byte instruction){
+    bool valid = true;
+    string registerName;
+    string instructionName;
+    byte* cpuRegister;
+
+    switch(instruction){
+        case CPU::INS_LDA_ABSY:
+            registerName = "Accumulator";
+            instructionName = "INS_LDA_ABSY";
+            cpuRegister = &cpu.Accumulator;
+            break;
+
+        case CPU::INS_LDX_ABSY:
+            registerName = "registerX";
+            instructionName = "INS_LDX_ABSY";
+            cpuRegister = &cpu.registerX;
+            break;
+
+        default:
+            cout << "load_register_absolute_Y doesn't handle : " << hex << (int)instruction << endl;
+            return false;
+    }
+    // Reseting cpu
+    cpu.reset(mem);
+    
+    CPU CpuCopy = cpu;
+
+    mem.write(0x0200, instruction);
+    mem.write(0x0201, 0x10);
+    mem.write(0x0202, 0x10);
+    mem.write(0x101A, 0xF6);
+    cpu.registerY = 0x0A;
+
+    cpu.step_run(mem);
+
+    if(!expected_eq(cpuRegister, 0xF6, instructionName, registerName)) valid = false;
+    if(!expected_eq(cpu.ZeroFlag, false, instructionName, "ZeroFlag")) valid = false;
+    if(!expected_eq(cpu.NegativeFlag, true, instructionName, "NegativeFlag")) valid = false;
+    if(!load_register_not_changing_unexpected_flags(cpu, CpuCopy, instructionName)) valid = false;
+    
+
+    return valid;
+}
+
+
+
+
+
+// Expected
 
 bool expected_eq(bool value, bool expected, string instruction, string thing){
     if(value != expected){
--- src/unit_test.h
+++ src/unit_test.h
@@ -6,16 +6,22 @@ using namespace std;
 
 void run_all_test(CPU& cpu, Memory& mem);
 
-bool LDA_IM(CPU& cpu, Memory& mem);
-bool LDA_ZP(CPU& cpu, Memory& mem);
-bool LDA_ZPX(CPU& cpu, Memory& mem);
-bool LDA_ABS(CPU& cpu, Memory& mem);
-bool LDA_ABSX(CPU& cpu, Memory& mem);
-bool LDA_ABSY(CPU& cpu, Memory& mem);
+
+// Load register test
+bool load_register_immediate(CPU& cpu, Memory& mem, byte instruction);
+bool load_register_zero_page(CPU& cpu, Memory& mem, byte instruction);
+bool load_register_zero_page_X(CPU& cpu, Memory& mem, byte instruction);
+bool load_register_absolute(CPU& cpu, Memory& mem, byte instruction);
+bool load_register_absolute_X(CPU& cpu, Memory& mem, byte instruction);
+bool load_register_absolute_Y(CPU& cpu, Memory& mem, byte instruction);
+
+// LDA
 bool LDA_INDX(CPU& cpu, Memory& mem);
 bool LDA_INDY(CPU& cpu, Memory& mem);
 
-bool load_register_not_changing_unexpected_flags(CPU& cpu, CPU& copy, string instruction);
 
+// Expected
+
+bool load_register_not_changing_unexpected_flags(CPU& cpu, CPU& copy, string instruction);
 bool expected_eq(bool value, bool expected, string instruction, string thing);
 bool expected_eq(byte value, byte expected, string instruction, string thing);
